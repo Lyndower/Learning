4ed4220c1b24dcff146a1fb2a8b53242a19f6e1a
Lyndower <141918626+Lyndower@users.noreply.github.com>
2024-01-01 14:14:07 +0800

    Update file.'w+'.py

--- file.'w+'.py
+++ file.'w+'.py
@@ -5,6 +5,8 @@ print(f1)
 f2=open('file2.txt','w+')#'w+'打开一个文件用于读写。如果该文件已存在则打开文件，并从开头开始编辑，即原有内容会被删除。如果该文件不存在，创建新文件。
 for i in range(len(f1),0,-1):#循环逆序读取行，将字符串输入新文件
     f2.write(f1[i-1])
+
+#查看输出file2.txt内容
 f2.seek(0)
 f2.write('\nTset')#测试输入字符串
 print(f2.read())
